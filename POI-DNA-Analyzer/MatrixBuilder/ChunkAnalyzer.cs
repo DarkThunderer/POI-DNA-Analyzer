59bcd84fba1afed49e82f8496bb378f9aa30dc0e
DESKTOP-4I3FVCN\AHTOH <antonmasharov@gmail.com>
2024-04-24 16:32:39 +0500

    feat: probability matrixes branch initialization

---
+++ POI-DNA-Analyzer/MatrixBuilder/ChunkAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿namespace POI_DNA_Analyzer
+{
+	internal class ChunkAnalyzer
+	{
+		public Dictionary<string, int> Pairs { get; private set; } = new Dictionary<string, int>();
+
+		public ChunkAnalyzer()
+		{
+			Pairs.Add("AA", 0);
+			Pairs.Add("AG", 0);
+			Pairs.Add("AC", 0);
+			Pairs.Add("AT", 0);
+
+			Pairs.Add("GA", 0);
+			Pairs.Add("GG", 0);
+			Pairs.Add("GC", 0);
+			Pairs.Add("GT", 0);
+
+			Pairs.Add("CA", 0);
+			Pairs.Add("CG", 0);
+			Pairs.Add("CC", 0);
+			Pairs.Add("CT", 0);
+
+			Pairs.Add("TA", 0);
+			Pairs.Add("TG", 0);
+			Pairs.Add("TC", 0);
+			Pairs.Add("TT", 0);
+		}
+
+		public void AnalyzeChunk(string chunk)
+		{
+			ClearPairs();
+
+			for (int i = 0; i < chunk.Length - 1; i++)
+			{
+				string pair = chunk[i].ToString() + chunk[i + 1].ToString();
+
+				HandlePair(pair);
+			}
+		}
+
+		private void HandlePair(string pair)
+		{
+			if (Pairs.ContainsKey(pair))
+				Pairs[pair]++;
+		}
+
+		private void ClearPairs()
+		{
+			foreach (string key in Pairs.Keys.ToList())
+				Pairs[key] = 0;
+		}
+	}
+}
---
+++ POI-DNA-Analyzer/MatrixBuilder/MatrixBuilder.cs
@@ -0,0 +1,60 @@
+﻿namespace POI_DNA_Analyzer
+{
+	internal class MatrixBuilder
+	{
+		private Dictionary<string, int> _pairs = new Dictionary<string, int>();
+
+		private int _anCount = 0;
+		private int _gnCount = 0;
+		private int _cnCount = 0;
+		private int _tnCount = 0;
+
+		public Dictionary<string, float> PairsProbabilities { get; private set; } = new Dictionary<string, float>();
+
+		public MatrixBuilder()
+		{
+			PairsProbabilities.Add("AA", 0);
+			PairsProbabilities.Add("AG", 0);
+			PairsProbabilities.Add("AC", 0);
+			PairsProbabilities.Add("AT", 0);
+
+			PairsProbabilities.Add("GA", 0);
+			PairsProbabilities.Add("GG", 0);
+			PairsProbabilities.Add("GC", 0);
+			PairsProbabilities.Add("GT", 0);
+
+			PairsProbabilities.Add("CA", 0);
+			PairsProbabilities.Add("CG", 0);
+			PairsProbabilities.Add("CC", 0);
+			PairsProbabilities.Add("CT", 0);
+
+			PairsProbabilities.Add("TA", 0);
+			PairsProbabilities.Add("TG", 0);
+			PairsProbabilities.Add("TC", 0);
+			PairsProbabilities.Add("TT", 0);
+		}
+
+		public void Bulid(Dictionary<string, int> pairs, int chunkSize)
+		{
+			_pairs = pairs;
+			ClearPairs();
+
+			foreach (string key in PairsProbabilities.Keys.ToList())
+			{
+				HandlePair(key, chunkSize);
+			}
+		}
+
+		private void HandlePair(string pair, int chunkSize)
+		{
+			if (PairsProbabilities.ContainsKey(pair))
+				PairsProbabilities[pair] = _pairs[pair] / chunkSize;
+		}
+
+		private void ClearPairs()
+		{
+			foreach (string key in PairsProbabilities.Keys.ToList())
+				PairsProbabilities[key] = 0;
+		}
+	}
+}
