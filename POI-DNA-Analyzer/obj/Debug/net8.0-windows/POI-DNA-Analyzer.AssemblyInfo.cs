a62364edb5146b58bec993441527b60596d9f2af
Anton Masharov <aommminc@gmail.com>
2024-04-24 19:57:30 +0500

    fix: layot fix

--- POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
+++ POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59bcd84fba1afed49e82f8496bb378f9aa30dc0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab008dbd91f3db96cf1daf1f876c0aeca80f05f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
