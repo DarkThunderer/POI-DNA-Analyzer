1706ad4b8845183634ea9df5e92db91ad163e4eb
Anton Masharov <aommminc@gmail.com>
2024-04-30 19:17:45 +0500

    feat: try to implement graphs

--- POI-DNA-Analyzer/MainWindow.xaml.cs
+++ POI-DNA-Analyzer/MainWindow.xaml.cs
@@ -19,6 +19,20 @@ namespace POI_DNA_Analyzer
 			_resultText = new ResultText(ResultText);
 			_listOfIndexes = new ListOfIndexes(List);
 			_sequencesFinder = new SequencesFinder();
+
+			ScottPlotProbabilityGraph dinucleotidesGraph = new ScottPlotProbabilityGraph(WpfPlot1);
+			dinucleotidesGraph.Show(new int[]{ 1, 2, 3, 4, 5 }, new double[]{ 0.52, 0.73, 0.28, 0.89, 0.41 }, System.Drawing.Color.Red);
+			dinucleotidesGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.14, 0.67, 0.92, 0.36, 0.75 }, System.Drawing.Color.Green);
+			dinucleotidesGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.81, 0.25, 0.63, 0.49, 0.97 }, System.Drawing.Color.Blue);
+			dinucleotidesGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.33, 0.68, 0.17, 0.94, 0.55 }, System.Drawing.Color.Orange);
+
+			InitializeComponent();
+			OxyPlotProbabilityGraph oxyPlotProbabilityGraph = new OxyPlotProbabilityGraph(OxyPlot);
+			oxyPlotProbabilityGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.52, 0.73, 0.28, 0.89, 0.41 }, System.Drawing.Color.Red);
+			oxyPlotProbabilityGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.14, 0.67, 0.92, 0.36, 0.75 }, System.Drawing.Color.Green);
+			oxyPlotProbabilityGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.81, 0.25, 0.63, 0.49, 0.97 }, System.Drawing.Color.Blue);
+			oxyPlotProbabilityGraph.Show(new int[] { 1, 2, 3, 4, 5 }, new double[] { 0.33, 0.68, 0.17, 0.94, 0.55 }, System.Drawing.Color.Orange);
+			oxyPlotProbabilityGraph.Show();
 		}
 
 		private void FindButtonClick(object sender, RoutedEventArgs e)
@@ -73,6 +87,16 @@ namespace POI_DNA_Analyzer
 			FileOpener fileOpener = new FileOpener();
 
 			_fileStream = fileOpener.OpenFile(_filePath);
+		}
+
+		private void f(object sender, RoutedEventArgs e)
+		{
+
+        }
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
 		}
 	}
 }
---
+++ POI-DNA-Analyzer/ProbabilityGraph/OxyPlotProbabilityGraph.cs
@@ -0,0 +1,49 @@
+﻿using OxyPlot.Series;
+using OxyPlot;
+using System.Drawing;
+using OxyPlot.Wpf;
+
+namespace POI_DNA_Analyzer
+{
+	class OxyPlotProbabilityGraph : IProbabilityGraph
+	{
+		private PlotView _plotView;
+		private PlotModel _model;
+
+		public OxyPlotProbabilityGraph(PlotView plotView)
+		{
+			_plotView = plotView;
+			_model = new PlotModel { };
+		}
+
+		public void Clear()
+		{
+			_model.Series.Clear();
+		}
+
+		public void Show(int[] chunks, double[] probabilities, Color color)
+		{
+			LineSeries series = new LineSeries
+			{
+				MarkerType = MarkerType.Circle,
+				MarkerSize = 4,
+				MarkerStroke = OxyColors.White,
+				MarkerFill = OxyColors.Blue,
+				Color = OxyColor.FromRgb(color.R, color.G, color.B),
+				StrokeThickness = 2,
+			};
+
+			for (int i = 0; i < chunks.Length; i++)
+			{
+				series.Points.Add(new DataPoint(chunks[i], probabilities[i]));
+			}
+
+			_model.Series.Add(series);
+		}
+
+		public void Show()
+		{
+			_plotView.Model = _model;
+		}
+	}
+}
